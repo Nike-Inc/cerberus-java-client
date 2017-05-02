4d1361b123962cd029d4e2f0884b41c9814b94be
Todd Lisonbee <tlisonbee@users.noreply.github.com>
2017-05-02 10:44:28 -0700

    Dependency upgrade and more logging (#18)
    * Adding more logging
    * Updating dependencies
    * Increasing version to 2.2.0

--- src/main/java/com/nike/cerberus/client/auth/aws/BaseAwsCredentialsProvider.java
+++ src/main/java/com/nike/cerberus/client/auth/aws/BaseAwsCredentialsProvider.java
@@ -103,8 +103,8 @@ public abstract class BaseAwsCredentialsProvider implements VaultCredentialsProv
     public BaseAwsCredentialsProvider(UrlResolver urlResolver) {
         super();
         this.urlResolver = urlResolver;
-
         cerberusJavaClientHeaderValue = ClientVersion.getClientHeaderValue();
+        LOGGER.info("Cerberus URL={}", urlResolver.resolve());
     }
 
     /**
@@ -117,7 +117,17 @@ public abstract class BaseAwsCredentialsProvider implements VaultCredentialsProv
     public VaultCredentials getCredentials() {
         readLock.lock();
         try {
-            if (credentials == null || expireDateTime.isBeforeNow()) {
+            boolean needsToAuthenticate = false;
+            if (credentials == null) {
+                // initial state: no credentials
+                needsToAuthenticate = true;
+            }
+            else if (expireDateTime.isBeforeNow()) {
+                // credentials have expired
+                needsToAuthenticate = true;
+                LOGGER.info("Cerberus credentials have expired {}, re-authenticating...", expireDateTime);
+            }
+            if (needsToAuthenticate) {
                 // Release the read lock and acquire a write lock
                 readLock.unlock();
                 writeLock.lock();
@@ -217,10 +227,11 @@ public abstract class BaseAwsCredentialsProvider implements VaultCredentialsProv
             final String key = "auth_data";
 
             if (authData.containsKey(key)) {
+                LOGGER.info(String.format("Authentication successful with AWS IAM principal ARN [%s] against [%s]",
+                        iamPrincipalArn, url));
                 return authData.get(key);
             } else {
-                throw new VaultClientException(
-                        "Success response from IAM role authenticate endpoint missing auth data!");
+                throw new VaultClientException("Success response from IAM role authenticate endpoint missing auth data!");
             }
         } catch (IOException e) {
             throw new VaultClientException("I/O error while communicating with Cerberus", e);
